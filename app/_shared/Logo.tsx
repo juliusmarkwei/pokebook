1366bd5a97711ca70027d938d3218a19346adfca
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-06-08 18:57:22 +0000

    added top & left properties

--- app/_shared/Logo.tsx
+++ app/_shared/Logo.tsx
@@ -1,11 +1,18 @@
 import _Logo from "@/public/pokemon-3.svg";
 import Image from "next/image";
 import { LogoPropType } from "../_types/propTypes";
+import { FC } from "react";
 
-const Logo = ({ height, width }: LogoPropType) => {
+const Logo: FC<LogoPropType> = ({ top, left, height, width }) => {
 	return (
 		<>
-			<Image src={_Logo} alt="pokemon" width={width} height={height} />
+			<Image
+				src={_Logo}
+				alt="pokemon"
+				width={width}
+				height={height}
+				className={`top-${top} left-${left}`}
+			/>
 		</>
 	);
 };
