6cd273fdab30eeca51ab3d8d06a4e831abdfd8eb
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-06-08 21:14:58 +0000

    pagination component

---
+++ app/components/ListView/Pagination.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Pagination = () => {
+	return (
+		<div className="absolute top-[560px] left-28 h-[40px] w-[366px] bg-slate-400 flex flex-row gap-2"></div>
+	);
+};
+
+export default Pagination;
