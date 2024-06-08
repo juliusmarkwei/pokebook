aa7687e4e0d37f489a8fafdb11b909ef4be27230
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-06-08 19:52:36 +0000

    component to lsit pokemon cards

---
+++ app/components/ListView/PokemonList.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const PokemonList = () => {
+	return (
+		<section className="border-2 border-pink-400 h-96 w-[85%] absolute top-32 left-28"></section>
+	);
+};
+
+export default PokemonList;
